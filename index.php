bbbde1d45b98972e5e23e193a3e4bb239206d800
anshika jain <nisthajain2412@gmail.com>
2022-05-22 13:27:06 +0530

    updated

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>College Management System</title>
+    <link rel="stylesheet" href="index.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+</head>
+<body>
+    <nav>
+        <label class="logo">X College</label>
+        <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">Admission</a></li>
+            <li><a href="">Contact Us</a></li>
+            <li><a href="" class="btn btn-success">Login</a></li>
+        </ul>
+    </nav>
+    <div class="section1">
+         <img src="./Assests/istockphoto-1142918319-612x612.jpg" alt="">
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+ 
+            </div>
+            <div class="col-md-8">
+
+            </div>
+        </div>
+        
+    </div>
+</body>
+</html>
