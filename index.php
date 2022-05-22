0a15045f4d594ce154af3c2d7e92382355c34c01
anshika jain <nisthajain2412@gmail.com>
2022-05-22 14:41:37 +0530

    updated

--- index.php
+++ index.php
@@ -1,14 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>College Management System</title>
     <link rel="stylesheet" href="index.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css"
+        integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap-theme.min.css"
+        integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.min.js"
+        integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous">
+    </script>
+
 </head>
+
 <body>
     <nav>
         <label class="logo">X College</label>
@@ -16,22 +29,98 @@
             <li><a href="">Home</a></li>
             <li><a href="">Admission</a></li>
             <li><a href="">Contact Us</a></li>
-            <li><a href="" class="btn btn-success">Login</a></li>
+            <li><a href="./loginPage/login.php" class="btn btn-success">Login</a></li>
         </ul>
     </nav>
     <div class="section1">
-         <img src="./Assests/istockphoto-1142918319-612x612.jpg" alt="">
+        <img src="./Assests/istockphoto-1142918319-612x612.jpg" alt="">
     </div>
     <div class="container">
         <div class="row">
             <div class="col-md-4">
- 
+
+                <img src="./Assests/school2.jpg" alt="">
             </div>
             <div class="col-md-8">
-
+                <h1>Welcome to X College</h1>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora harum vero laudantium eum quae aut
+                    esse ad ipsa cumque, eligendi blanditiis recusandae molestiae adipisci velit? Sed, cum molestiae aut
+                    suscipit modi minima voluptates sequi nam corporis eveniet harum eos id. Voluptates corporis
+                    distinctio beatae quasi ad magnam a culpa dolores exercitationem adipisci hic autem, laboriosam
+                    ipsam. Dolorem quia vitae consectetur porro fugiat molestiae nisi voluptatum maiores magnam nihil
+                    fugit, quibusdam eos nam tenetur neque delectus quod blanditiis quae beatae aliquam illo veniam
+                    libero laboriosam autem? Velit at pariatur facilis inventore provident neque illo, voluptates
+                    dolorum tempora, dolores, necessitatibus ipsam. Magnam.</p>
             </div>
         </div>
-        
+
     </div>
+    <div class="Center">
+        <h1>Our Teachers</h1>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <img class="teacher" src="./Assests/teacher1.jpg" alt="">
+                <p>in a variant, academically challenging and encouraging environment where manifold viewpoints are
+                    prized and celebrated</p>
+            </div>
+            <div class="col-md-4">
+                <img class="teacher" src="./Assests/teacher2.jpg" alt="">
+                <p>in a variant, academically challenging and encouraging environment where manifold viewpoints are
+                    prized and celebrated</p>
+            </div>
+            <div class="col-md-4">
+                <img class="teacher" src="./Assests/teacher3.jpg" alt="">
+                <p>in a variant, academically challenging and encouraging environment where manifold viewpoints are
+                    prized and celebrated</p>
+            </div>
+        </div>
+    </div>
+    <div class="Center">
+        <h1>Our Courses</h1>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <img class="teacher" src="./Assests/graphic.jpg" alt="">
+                <h3>Graphic Design</h3>
+            </div>
+            <div class="col-md-4">
+                <img class="teacher" src="./Assests/marketing.png" alt="">
+                <h3>Marketing</h3>
+            </div>
+            <div class="col-md-4">
+                <img class="teacher" src="./Assests/web.jpg" alt="">
+                <h3>Web Development</h3>
+            </div>
+        </div>
+    </div>
+    <div class="Center">
+        <h1>Admission Form</h1>
+    </div>
+    <div class="admission">
+        <form>
+            <div class="adm_int">
+                <label class="label_text">Name</label>
+                <input class="input_type" type="text" name="">
+            </div>
+            <div class="adm_int">
+                <label class="label_text">Email</label>
+                <input class="input_type" type="text" name="">
+            </div>
+            <div class="adm_int">
+                <label class="label_text">Message</label>
+                <textarea></textarea>
+            </div>
+            <div class="adm_int">
+                <input class="btn btn-primary" id="submit" type="submit" value="apply">
+            </div>
+        </form>
+    </div>
+    <footer>
+        <h3>All @copyright reserved by web tech knowledge</h3>
+    </footer>
 </body>
+
 </html>
---
+++ loginPage/login.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Form</title>
+    <link rel="stylesheet" href="login.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css"
+        integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap-theme.min.css"
+        integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.min.js"
+        integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous">
+    </script>
+</head>
+
+<body>
+    <div class="center">
+        <div class="form_deg">
+            <form>
+                <div>
+                    <label>Username</label>
+                    <input type="text" name="username">
+                </div>
+                <div>
+                    <label>Password</label>
+                    <input type="Password" name="password">
+                </div>
+                <div>
+
+                    <input class="btn btn-primary" type="submit" name="submit" value="login">
+                </div>
+            </form>
+        </div>
+    </div>
+</body>
+
+</html>
